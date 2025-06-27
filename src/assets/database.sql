40ad70fd7d8e58845caafad4ec87d3569756262e
Italo Valcy S Brito <italovalcy@gmail.com>
2025-06-27 12:23:42 -0300

    Update src/assets/database.sql

--- src/assets/database.sql
+++ src/assets/database.sql
@@ -410,7 +410,6 @@ INSERT INTO `commands` (`name`, `description`, `examples`, `tool`, `type`, `comm
 ('Subdomains scrapping', 'The maximum number of subdomains that will be scraped from Google (default 15)', 'dnsenum -s 40 example.com', 14, 'input', '-s', NULL, NULL, 'GOOGLE SCRAPPING OPTIONS: SUBDOMAINS'),
 ('Choose the brute force file', 'Read subdomains from this file to perform brute force. (Takes priority over default dns.txt)', 'dnsenum -f subdomains.txt example.com', 14, 'input', '-f', NULL, NULL, 'SUBDOMAIN BRUTE FORCE'), 
 ('Recursion on subdomains', 'Executes a brute force in all discovered subdomains that have an NS record.', NULL, 14, 'checkbox', '-r', NULL, NULL, 'BRUTE FORCE OPTIONS'),
-('PTR records exclusion', 'Exclude PTR records that match the regexp expression from reverse lookup results, useful on invalid hostnames. In this sense, when a reverse lookup is made, subdomains with the defined words are not considered.', 'dnsenum -e "(test|local|invalid)" examples.', 14, 'input', '-e', NULL, NULL, 'REVERSE LOOKUP OPTIONS'),
 ('Output in XML format (Choose the file)', 'Can be imported in MagicTree (www.gremwell.com).', 'dnsenum -o output.xml example.com', 14, 'input', '-o', NULL, NULL, 'OUTPUT OPTIONS');
 
 -- --------------------------------------------------------
